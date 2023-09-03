b0598e273dbd5cd1af3764066f23be799d53aa93
Akash Ghosh <93257481+akashghosh256@users.noreply.github.com>
2023-09-03 14:04:50 +0530

    Time: 6 ms (84.42%) | Memory: 43.5 MB (54.70%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,26 +1,65 @@
-import java.util.*;
-class Solution {
-    public String reverseWords(String s) {
- s = s.trim();
-    s = s.replaceAll("\\s+", " ");
-    // Split the input string into words using space as a delimiter
-        String[] words = s.split(" ");
+ import java.util.*;
+// class Solution {
+//     public String reverseWords(String s) {
+//  s = s.trim();
+//     s = s.replaceAll("\\s+", " ");
+//     // Split the input string into words using space as a delimiter
+//         String[] words = s.split(" ");
         
-        // Create a StringBuilder to store the reversed words
-        StringBuilder reversedString = new StringBuilder();
+//         // Create a StringBuilder to store the reversed words
+//         StringBuilder reversedString = new StringBuilder();
         
-        // Iterate through the words in reverse order and append them to the reversed string
-        for (int i = words.length - 1; i >= 0; i--) {
-            reversedString.append(words[i]);
+//         // Iterate through the words in reverse order and append them to the reversed string
+//         for (int i = words.length - 1; i >= 0; i--) {
+//             reversedString.append(words[i]);
             
-            // Add a space after each word, except for the last word
-            if (i > 0) {
-                reversedString.append(" ");
-            }
-        }
+//             // Add a space after each word, except for the last word
+//             if (i > 0) {
+//                 reversedString.append(" ");
+//             }
+//         }
 
-      return  reversedString.toString();
+//       return  reversedString.toString();
         
 
+//     }
+// }
+class Solution {
+public String reverseWords(String s) {
+        Stack<String> st = new Stack<String>();
+        for (String a : s.trim().split(" ")) {
+            if (!a.isEmpty())
+                st.push(a);
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        while (!st.isEmpty()) {
+            sb.append(st.pop());
+            sb.append(" ");            
+        }
+        
+        return sb.toString().trim();
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
