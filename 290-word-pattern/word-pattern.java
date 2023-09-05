1b67a37d8f8f252039010069ab23e9644fcf0d20
Akash Ghosh <93257481+akashghosh256@users.noreply.github.com>
2023-09-06 00:51:14 +0530

    Time: 2 ms (14.92%) | Memory: 40.6 MB (36.75%) - LeetSync

--- 290-word-pattern/word-pattern.java
+++ 290-word-pattern/word-pattern.java
@@ -2,14 +2,17 @@ import java.util.*;
 class Solution {
     public boolean wordPattern(String pattern, String s) {
         if(pattern == null || s == null) return false;
+        // converting String to words array for comparison
         String[] words = s.split("\\s+");
-        // for(String i : words)
-        //   System.out.println(i);
+  
 
-
+  //  pattern length should be equal to Number of words in string to follow the word pattern
         if(pattern.length() != words.length) return false;
 
+//  To have check of correct characters with word
         HashMap<Character,String> map = new HashMap();
+
+// To keep track and avoid duplicate string for different alphabets of pattern 
          Set<String> set = new HashSet<>();
 
         int i=0;
@@ -22,10 +25,14 @@ class Solution {
 
             }
             else{
-               if(set.contains(words[i])) return false;
+               if(set.contains(words[i])) return false; // if set already contains the word it means the word is also related to some other alphabet of pattern which is not possible
+
+               // add the word and character
                 map.put(pattern.charAt(i),words[i]);
                 set.add(words[i]);
             }
+
+            
             i++;
 
 
