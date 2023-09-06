56091caea8ce814a5c0c18a7d3ccdff762776903
Akash Ghosh <93257481+akashghosh256@users.noreply.github.com>
2023-09-06 23:20:50 +0530

    Time: 12 ms (34.74%) | Memory: 43.6 MB (49.44%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,15 +1,43 @@
+// class Solution {
+//     public boolean isAnagram(String s, String t) {
+//         // The idea is simple. It creates a size 26 int arrays as buckets for each letter in alphabet. It increments the bucket value with String s and decrement with string t. So if they are anagrams, all buckets should remain with initial value which is zero or if values gets lower than 0 then also return false. So just checking that and return
+//       int [] alphabet= new int[26];
+//         for (int i = 0; i < s.length(); i++) alphabet[s.charAt(i) - 'a']++;
+
+//         for (int i = 0; i < t.length(); i++) 
+//         {  
+//           int count = alphabet[t.charAt(i) - 'a']--;
+//            if( count  < 0 ) return false;
+           
+//        }
+//         for (int i : alphabet) if (i != 0) return false;
+//         return true;
+
+
+//     }
+// }
+
 class Solution {
     public boolean isAnagram(String s, String t) {
-        // The idea is simple. It creates a size 26 int arrays as buckets for each letter in alphabet. It increments the bucket value with String s and decrement with string t. So if they are anagrams, all buckets should remain with initial value which is zero. So just checking that and return
-      int [] alphabet= new int[26];
-        for (int i = 0; i < s.length(); i++) alphabet[s.charAt(i) - 'a']++;
-        for (int i = 0; i < t.length(); i++) 
-        {  if(alphabet[t.charAt(i) - 'a']--   < 0 ) return false;
-           
-       }
-          for (int i : alphabet) if (i != 0) return false;
+        Map<Character, Integer> count = new HashMap<>();
+        
+        // Count the frequency of characters in string s
+        for (char x : s.toCharArray()) {
+            count.put(x, count.getOrDefault(x, 0) + 1);
+        }
+        
+        // Decrement the frequency of characters in string t
+        for (char x : t.toCharArray()) {
+            count.put(x, count.getOrDefault(x, 0) - 1);
+        }
+        
+        // Check if any character has non-zero frequency
+        for (int val : count.values()) {
+            if (val != 0) {
+                return false;
+            }
+        }
+        
         return true;
-
-
     }
 }
