a1dc003cdba7e5b078f8cfa2341369f8cae65d22
Akash Ghosh <93257481+akashghosh256@users.noreply.github.com>
2023-09-06 13:02:29 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (71.62%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
@@ -2,9 +2,12 @@ class Solution {
     public int strStr(String haystack, String needle) {
         int n = needle .length();
         int m = haystack.length();
+    
 
         int i=0;
         while(i<m){
+        // if a character matches the any character of haystack then we use 
+        // substring to pick up the string and match with needle else we move ahead
             if(needle.charAt(0) == haystack.charAt(i) && (i+n <=m)){
                 if(needle.equals(haystack.substring(i,i+n))) return i;
             }
@@ -14,3 +17,6 @@ class Solution {
         
     }
 }
+
+// time complexity is O(m * n), where m is the length of the haystack string, and n is the length of the needle string.
+//  space complexity is O(n) for the substring created inside the loop.
