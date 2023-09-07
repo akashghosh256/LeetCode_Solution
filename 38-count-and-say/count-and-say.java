8271b7a26f64078d41aadd5e59cedd8c5f8cb442
Akash Ghosh <93257481+akashghosh256@users.noreply.github.com>
2023-09-07 11:17:27 +0530

    Time: 6 ms (53.39%) | Memory: 40.2 MB (65.05%) - LeetSync

--- 38-count-and-say/count-and-say.java
+++ 38-count-and-say/count-and-say.java
@@ -1,28 +1,58 @@
-class Solution {
-    public String countAndSay(int n) {
-        if( n == 1 ) return "1";
-        if( n == 2 ) return "11";
+// class Solution {
+//     public String countAndSay(int n) {
+//         if( n == 1 ) return "1";
+//         if( n == 2 ) return "11";
         
-        String num = "11";
-        for(int i=3;i<=n;i++){
-            String temp = "";
+//         String num = "11";
+//         for(int i=3;i<=n;i++){
+//             String temp = "";
+//             int count = 1;
+//             num = num + "$";
+//         for(int j=0;j < num.length()-1;j++){
+
+//             if(num.charAt(j) == num.charAt(j+1)) count++;
+
+//             else{
+
+//                 temp = temp + Integer.toString(count);
+//                 temp = temp + num.charAt(j);
+//                 count = 1;
+//             }
+//         }
+//         num = temp;
+
+//         }
+//         return num;
+
+//     }
+// }
+
+public class Solution {
+    public String countAndSay(int n) {
+        if (n == 1)
+            return "1";
+
+        StringBuilder num = new StringBuilder("1");
+
+        for (int i = 2; i <= n; i++) {
+            StringBuilder temp = new StringBuilder();
             int count = 1;
-            num = num + "$";
-        for(int j=0;j < num.length()-1;j++){
 
-            if(num.charAt(j) == num.charAt(j+1)) count++;
-
-            else{
-
-                temp = temp + Integer.toString(count);
-                temp = temp + num.charAt(j);
-                count = 1;
+            char currentChar = num.charAt(0);
+            for (int j = 1; j < num.length(); j++) {
+                if (num.charAt(j) == currentChar) {
+                    count++;
+                } else {
+                    temp.append(count).append(currentChar);
+                    currentChar = num.charAt(j);
+                    count = 1;
+                }
             }
-        }
-        num = temp;
 
+            temp.append(count).append(currentChar);
+            num = temp;
         }
-        return num;
 
+        return num.toString();
     }
 }
