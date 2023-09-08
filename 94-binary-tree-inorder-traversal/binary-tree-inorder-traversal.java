31ba2fe722255d3a0b2ac954ba9ccbcdca272aae
Akash Ghosh <93257481+akashghosh256@users.noreply.github.com>
2023-09-08 18:30:07 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (28.81%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
@@ -43,36 +43,89 @@
 // In the worst case, the function pushes all nodes from the root to the deepest leaf node onto the stack. The maximum number of nodes that can be on the stack at any given time is equal to the height of the tree.
 
 
+//  Recursive Approach 
+// import java.util.ArrayList;
+// import java.util.List;
 
-import java.util.ArrayList;
-import java.util.List;
+// public class Solution {
+//     public List<Integer> inorderTraversal(TreeNode root) {
+//         List<Integer> list = new ArrayList<Integer>();
+//         inorderRecursive(root, list);
+//         return list;
+//     }
 
+//     private void inorderRecursive(TreeNode node, List<Integer> list) {
+//         if (node == null) {
+//             return;
+//         }
+
+//         // Traverse the left subtree
+//         inorderRecursive(node.left, list);
+
+//         // Process the current node
+//         list.add(node.val);
+
+//         // Traverse the right subtree
+//         inorderRecursive(node.right, list);
+//     }
+// }
+
+
+import java.util.*;
 public class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> list = new ArrayList<Integer>();
-        inorderRecursive(root, list);
-        return list;
+
+List<Integer> ans =  new ArrayList<>();
+if (root == null) return ans;
+
+
+TreeNode current = root;
+		while (current != null)
+		{
+			if (current.left == null)
+			{
+				//System.out.print(current.data + " ");
+                ans.add(current.val);
+				current = current.right;
+			}
+			else {
+				/* Find the inorder
+					predecessor of current
+				*/
+			TreeNode	pre = current.left;
+				while (pre.right != null
+					&& pre.right != current)
+					pre = pre.right;
+
+				/* Make current as right
+				child of its
+				* inorder predecessor */
+				if (pre.right == null) {
+					pre.right = current;
+					current = current.left;
+				}
+
+				/* Revert the changes made
+				in the 'if' part
+				to restore the original
+				tree i.e., fix
+				the right child of predecessor*/
+				else
+				{
+					pre.right = null;
+					//System.out.print(current.data + " ");
+                    ans.add(current.val);
+					current = current.right;
+				} /* End of if condition pre->right == NULL
+				*/
+
+			} /* End of if condition current->left == NULL*/
+
+		} /* End of while */
+        return ans;
+
     }
 
-    private void inorderRecursive(TreeNode node, List<Integer> list) {
-        if (node == null) {
-            return;
-        }
-
-        // Traverse the left subtree
-        inorderRecursive(node.left, list);
-
-        // Process the current node
-        list.add(node.val);
-
-        // Traverse the right subtree
-        inorderRecursive(node.right, list);
-    }
 }
-
-
-
-
-
 
 
