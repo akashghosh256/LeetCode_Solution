102277a5419492bc89dea882802d536b2e123961
Akash Ghosh <93257481+akashghosh256@users.noreply.github.com>
2023-09-08 10:07:47 +0530

    Time: 1 ms (21.16%) | Memory: 40.6 MB (76.32%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
@@ -14,22 +14,44 @@
  * }
  */
  // Postorder - left - right - root
+
+
+ 
+// class Solution {
+// public List<Integer> postorderTraversal(TreeNode root) {
+//     List<Integer> list = new ArrayList<>();
+//     if(root == null) return list;
+//     Stack<TreeNode> stack = new Stack<>();
+//     stack.push(root);
+//     while(!stack.empty()){
+//         root = stack.pop();
+
+//         list.add(0, root.val);    // it is inserting the value at the front of the list, which effectively reverses the order of elements in the list.
+// //when you add a new element at index 0, it shifts all existing elements in the list to the right by one position. In other words, the element that was previously at index 0 is moved to index 1, the element at index 1 is moved to index 2, and so on. This makes room for the new element at index 0.
+
+
+//         if(root.left != null) stack.push(root.left);
+//         if(root.right != null) stack.push(root.right);
+//     }
+//     return list;
+// }
+// }
+
+
+import java.util.*;
+
 class Solution {
-public List<Integer> postorderTraversal(TreeNode root) {
-    List<Integer> list = new ArrayList<>();
-    if(root == null) return list;
-    Stack<TreeNode> stack = new Stack<>();
-    stack.push(root);
-    while(!stack.empty()){
-        root = stack.pop();
-
-        list.add(0, root.val);    // it is inserting the value at the front of the list, which effectively reverses the order of elements in the list.
-//when you add a new element at index 0, it shifts all existing elements in the list to the right by one position. In other words, the element that was previously at index 0 is moved to index 1, the element at index 1 is moved to index 2, and so on. This makes room for the new element at index 0.
-
-
-        if(root.left != null) stack.push(root.left);
-        if(root.right != null) stack.push(root.right);
+    public List<Integer> postorderTraversal(TreeNode root) {
+        LinkedList<Integer> list = new LinkedList<>();
+        if (root == null) return list;
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.empty()) {
+            root = stack.pop();
+            list.addFirst(root.val); // Add to the beginning of the list
+            if (root.left != null) stack.push(root.left);
+            if (root.right != null) stack.push(root.right);
+        }
+        return list;
     }
-    return list;
-}
 }
