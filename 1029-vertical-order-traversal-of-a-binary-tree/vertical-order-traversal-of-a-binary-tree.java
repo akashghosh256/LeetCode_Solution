59729e726c6da4072c06ce31056cf66f46f961ef
Akash Ghosh <93257481+akashghosh256@users.noreply.github.com>
2023-09-10 12:49:01 +0530

    Time: 4 ms (63.66%) | Memory: 41.8 MB (38.50%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.java
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.java
@@ -16,9 +16,10 @@
 
 
 class Tuple {
-    TreeNode node;
-    int scale;
-    int level;
+    TreeNode node;  // TreeNode representing the current node in the tree.
+    int scale;      // Horizontal position or "x" coordinate of the node.
+    int level;      // Vertical position or "y" coordinate of the node.
+
     public Tuple(TreeNode _node, int _scale, int _level) {
         node = _node;
         scale = _scale;
@@ -29,25 +30,34 @@ class Tuple {
 class Solution {
     public List<List<Integer>> verticalTraversal(TreeNode root) {
 
+        // TreeMap to store nodes by their horizontal position (scale).
+        // The inner TreeMap stores nodes by their vertical position (level),
+        // and PriorityQueue is used to maintain nodes within the same level.
+        TreeMap<Integer, TreeMap<Integer, PriorityQueue<Integer>>> map = new TreeMap<>();
 
-TreeMap < Integer, TreeMap < Integer, PriorityQueue < Integer >>> map = new TreeMap < > ();
-    Queue < Tuple > q = new LinkedList < Tuple > ();
-        q.offer(new Tuple(root, 0, 0));
+        // Queue for level-order traversal of the tree.
+        Queue<Tuple> q = new LinkedList<>();
+        q.offer(new Tuple(root, 0, 0)); // Start with the root node at (0,0).
+
+        // Perform level-order traversal of the tree.
         while (!q.isEmpty()) {
             Tuple tuple = q.poll();
             TreeNode node = tuple.node;
-            int x = tuple.scale;
-            int y = tuple.level;
+            int x = tuple.scale;  // Horizontal position.
+            int y = tuple.level;  // Vertical position.
 
-
+            // Create necessary data structures for the current horizontal position.
             if (!map.containsKey(x)) {
-                map.put(x, new TreeMap < > ());
+                map.put(x, new TreeMap<>());
             }
             if (!map.get(x).containsKey(y)) {
-                map.get(x).put(y, new PriorityQueue < > ());
+                map.get(x).put(y, new PriorityQueue<>());
             }
+
+            // Add the node value to the appropriate PriorityQueue.
             map.get(x).get(y).offer(node.val);
 
+            // Enqueue left and right children for further traversal.
             if (node.left != null) {
                 q.offer(new Tuple(node.left, x - 1, y + 1));
             }
@@ -55,17 +65,19 @@ TreeMap < Integer, TreeMap < Integer, PriorityQueue < Integer >>> map = new Tree
                 q.offer(new Tuple(node.right, x + 1, y + 1));
             }
         }
-        List < List < Integer >> list = new ArrayList < > ();
-        for (TreeMap < Integer, PriorityQueue < Integer >> ys: map.values()) {
-            list.add(new ArrayList < > ());
-            for (PriorityQueue < Integer > nodes: ys.values()) {
+
+        // Prepare the final result list by flattening the TreeMap structure.
+        List<List<Integer>> list = new ArrayList<>();
+        for (TreeMap<Integer, PriorityQueue<Integer>> ys : map.values()) {
+            list.add(new ArrayList<>());
+            for (PriorityQueue<Integer> nodes : ys.values()) {
                 while (!nodes.isEmpty()) {
                     list.get(list.size() - 1).add(nodes.poll());
                 }
             }
         }
+
         return list;  
-   
     }
 }
 
