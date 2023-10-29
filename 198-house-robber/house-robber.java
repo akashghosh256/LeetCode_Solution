1ef65f5b874aab3d2281a3742b536433ab91b83c
Akash Ghosh <93257481+akashghosh256@users.noreply.github.com>
2023-10-29 22:19:09 +0530

    Time: 0 ms (100.00%) | Memory: 39.8 MB (55.32%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -20,26 +20,69 @@
 //     }
 // }
 
+
+// Memoization
+// class Solution {
+
+//     private int f(int[] a, int l, int[] dp){
+//         if( l==0) return a[0];
+//         if( l<0 ) return 0;
+//         if( dp[l] != -1) return dp[l];
+//         int notPick = 0 + f(a,l-1,dp);
+//         int pick = a[l] + f(a,l-2,dp);
+
+//         return dp[l] = Math.max(pick,notPick);
+
+
+
+//     }
+
+
+//     public int rob(int[] nums) {
+//         int l = nums.length;
+//         int [] dp = new int[l];
+//         Arrays.fill(dp,-1);
+
+
+
+//          return f(nums,l-1,dp);
+
+//     }
+// }
+
+// Tabulation
+
 class Solution {
 
-    private int f(int[] a, int l, int[] dp){
-        if( l==0) return a[0];
-        if( l<0 ) return 0;
-        if( dp[l] != -1) return dp[l];
-        int notPick = 0 + f(a,l-1,dp);
-        int pick = a[l] + f(a,l-2,dp);
+    private int f(int[] nums, int l, int[] dp){
+    dp[0] = nums[0];
 
-        return dp[l] = Math.max(pick,notPick);
+    for( int i =1; i<=l; i++){
+
+ int pick = nums[i];
+
+ if( i>1 )
+ pick  +=  dp[i-2];
+
+
+ int notPick = dp[i-1];
+
+ dp[i] = Math.max(pick,notPick); 
 
 
 
     }
+return dp[l];
+    }
+    
 
 
     public int rob(int[] nums) {
         int l = nums.length;
         int [] dp = new int[l];
         Arrays.fill(dp,-1);
+
+    
 
 
 
