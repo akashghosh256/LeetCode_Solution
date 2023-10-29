db442236ee3c90be8498e0f413a2203c1406056c
Akash Ghosh <93257481+akashghosh256@users.noreply.github.com>
2023-10-29 22:28:42 +0530

    Time: 0 ms (100.00%) | Memory: 39.8 MB (55.32%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -52,41 +52,83 @@
 
 // Tabulation
 
+// class Solution {
+
+//     private int f(int[] nums, int l, int[] dp){
+//     dp[0] = nums[0];
+
+//     for( int i =1; i<=l; i++){
+
+//  int pick = nums[i];
+
+//  if( i>1 )
+//  pick  +=  dp[i-2];
+
+
+//  int notPick = dp[i-1];
+
+//  dp[i] = Math.max(pick,notPick); 
+
+
+
+//     }
+// return dp[l];
+//     }
+    
+
+
+//     public int rob(int[] nums) {
+//         int l = nums.length;
+//         int [] dp = new int[l];
+//         Arrays.fill(dp,-1);
+
+    
+
+
+
+//          return f(nums,l-1,dp);
+
+//     }
+// }
+
+
+// Space Memoization
+
 class Solution {
 
-    private int f(int[] nums, int l, int[] dp){
-    dp[0] = nums[0];
-
-    for( int i =1; i<=l; i++){
-
- int pick = nums[i];
-
- if( i>1 )
- pick  +=  dp[i-2];
-
-
- int notPick = dp[i-1];
-
- dp[i] = Math.max(pick,notPick); 
-
-
-
-    }
-return dp[l];
-    }
     
 
 
-    public int rob(int[] nums) {
-        int l = nums.length;
-        int [] dp = new int[l];
-        Arrays.fill(dp,-1);
+    public int rob(int[] arr) {
+        int n = arr.length;
+    int prev = arr[0];
+        int prev2 = 0;
+
+        // Iterate through the array starting from the second element.
+        for (int i = 1; i < n; i++) {
+            // Calculate the maximum sum by either picking the current element or not picking it.
+            int pick = arr[i];
+
+            // If there are at least two elements before the current element, add the value from prev2.
+            if (i > 1)
+                pick += prev2;
+
+            // The non-pick option is to use the maximum sum from the previous position.
+            int nonPick = prev;
+
+            // Calculate the maximum sum for the current position and update prev and prev2.
+            int cur_i = Math.max(pick, nonPick);
+            prev2 = prev;
+            prev = cur_i;
+        }
+
+        // The 'prev' variable now holds the maximum possible sum.
+        return prev;
 
     
 
 
 
-         return f(nums,l-1,dp);
 
     }
 }
