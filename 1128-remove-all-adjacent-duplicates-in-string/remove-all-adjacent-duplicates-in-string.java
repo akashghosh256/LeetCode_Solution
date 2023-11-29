73da74eaebe6171f15f5da545d57a01e3205a0e6
Akash Ghosh <93257481+akashghosh256@users.noreply.github.com>
2023-11-29 09:35:27 +0530

    Time: 4 ms (99.67%) | Memory: 43.9 MB (94.20%) - LeetSync

--- 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.java
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.java
@@ -1,23 +1,45 @@
+// Two pointerrrrrrrrrrrrrrrrrrrrrrrrrrrrrr
+
 class Solution {
     public String removeDuplicates(String s) {
-        int n = s.length();
-        if(n <= 1) return s;
-        Stack<Character> st = new Stack<>();
-   
-        for(int i=0; i<n;i++){
-            if(st.isEmpty()) st.push(s.charAt(i));
-            else{
-                if(st.peek() == s.charAt(i)){
-                    st.pop();
-                }
-                else st.push(s.charAt(i));
-            }
+        int n = s.length(), i=0;
+char res[] = s.toCharArray();
+for( int j=0; j<n;j++,i++){
+    res[i]= res[j];
+    if(i>0 && res[i] == res[i-1]) i-=2;
+}
 
-        }
-	StringBuilder str = new StringBuilder();
-    while(!st.isEmpty()) str.append(st.pop());
-    str.reverse();
-    return (str.toString());
+return new String(res, 0, i);
+
 
     }
 }
+
+
+
+
+
+// stack-----------------------------
+// class Solution {
+//     public String removeDuplicates(String s) {
+//         int n = s.length();
+//         if(n <= 1) return s;
+//         Stack<Character> st = new Stack<>();
+   
+//         for(int i=0; i<n;i++){
+//             if(st.isEmpty()) st.push(s.charAt(i));
+//             else{
+//                 if(st.peek() == s.charAt(i)){
+//                     st.pop();
+//                 }
+//                 else st.push(s.charAt(i));
+//             }
+
+//         }
+// 	StringBuilder str = new StringBuilder();
+//     while(!st.isEmpty()) str.append(st.pop());
+//     str.reverse();
+//     return (str.toString());
+
+//     }
+// }
