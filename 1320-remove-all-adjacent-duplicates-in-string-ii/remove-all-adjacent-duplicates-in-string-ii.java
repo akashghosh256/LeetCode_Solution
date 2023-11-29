318aa26494d70bebc9511e5005790446e03094ac
Akash Ghosh <93257481+akashghosh256@users.noreply.github.com>
2023-11-29 11:34:02 +0530

    Time: 5 ms (99.47%) | Memory: 44.9 MB (39.93%) - LeetSync

--- 1320-remove-all-adjacent-duplicates-in-string-ii/remove-all-adjacent-duplicates-in-string-ii.java
+++ 1320-remove-all-adjacent-duplicates-in-string-ii/remove-all-adjacent-duplicates-in-string-ii.java
@@ -1,84 +1,80 @@
 
 
-class Solution {
-  private static class Pair{
-   char s;
-    int v;
-   public Pair(char s, int v){
-       this.s = s;
-       this.v = v;
-   }
+// class Solution {
+//   private static class Pair{
+//    char s;
+//     int v;
+//    public Pair(char s, int v){
+//        this.s = s;
+//        this.v = v;
+//    }
 
-   public char getKey(){
-       return s;
-   }
+//    public char getKey(){
+//        return s;
+//    }
 
-   public int getValue(){
-       return v;
-   }
-}
-    public String removeDuplicates(String s, int k) {
-        int n = s.length();
-        Stack<Pair> st= new Stack<>();
-        for(int i=0; i<n; i++){
-        if(st.isEmpty() || st.peek().getKey() != s.charAt(i)){
-            st.push(new Pair(s.charAt(i), 1));
-        }
-        else{
-             Pair prev = st.pop();
-            st.push(new Pair(s.charAt(i), prev.getValue() + 1));
-        }
+//    public int getValue(){
+//        return v;
+//    }
+// }
+//     public String removeDuplicates(String s, int k) {
+//         int n = s.length();
+//         Stack<Pair> st= new Stack<>();
+//         for(int i=0; i<n; i++){
+//         if(st.isEmpty() || st.peek().getKey() != s.charAt(i)){
+//             st.push(new Pair(s.charAt(i), 1));
+//         }
+//         else{
+//              Pair prev = st.pop();
+//             st.push(new Pair(s.charAt(i), prev.getValue() + 1));
+//         }
 
-        if( st.peek().getValue() == k) st.pop();
+//         if( st.peek().getValue() == k) st.pop();
 
-        }
-        StringBuilder ans = new StringBuilder();
+//         }
+//         StringBuilder ans = new StringBuilder();
 
-        while(!st.isEmpty()){
-            Pair cur = st.pop();
-            int count = cur.getValue();
-        while(count-- > 0){
-            ans.append(cur.getKey());
-        }
+//         while(!st.isEmpty()){
+//             Pair cur = st.pop();
+//             int count = cur.getValue();
+//         while(count-- > 0){
+//             ans.append(cur.getKey());
+//         }
        
 
-        }      
+//         }      
     
 
-return ans.reverse().toString();
-    }
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// class Solution {
-//     public String removeDuplicates(String s, int k) {
-//        int i = 0, n = s.length(), count[] = new int[n];
-//         char[] stack = s.toCharArray();
-//         for (int j = 0; j < n; ++j, ++i) {
-//             stack[i] = stack[j];
-//             count[i] = i > 0 && stack[i - 1] == stack[j] ? count[i - 1] + 1 : 1;
-//             if (count[i] == k) i -= k;
-//         }
-//         return new String(stack, 0, i);  
+// return ans.reverse().toString();
 //     }
+
 // }
 
 
+
+
+
+
+
+
+
+
+
+
+
+// two pointer---------------------------
+class Solution {
+    public String removeDuplicates(String s, int k) {
+        int i = 0, n = s.length(), count[] = new int[n];
+        char[] arr = s.toCharArray();
+        for (int j = 0; j < n; ++j, ++i) {
+            arr[i] = arr[j];
+            count[i] = i > 0 && arr[i - 1] == arr[j] ? count[i - 1] + 1 : 1;
+            if (count[i] == k) i -= k;
+        }
+        return new String(arr, 0, i);
+    }
+} 
 
 
 
