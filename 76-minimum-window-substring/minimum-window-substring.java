58c09e7884a89420ce608d434f62f22fec5dac19
Akash Ghosh <93257481+akashghosh256@users.noreply.github.com>
2024-01-07 16:12:58 +0530

    Time: 2 ms (98.19%) | Memory: 43.7 MB (87.53%) - LeetSync

---
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -0,0 +1,27 @@
+// class Solution {
+//     public String minWindow(String s, String t) {
+        
+//     }
+// }
+
+
+public class Solution {
+
+    public String minWindow(String s, String t) {
+        int[] map = new int[128];
+        for (char c : t.toCharArray()) map[c]++;
+        
+        int counter = t.length(), begin = 0, end = 0, d = Integer.MAX_VALUE, head = 0;
+        
+        while (end < s.length()) {
+            if (map[s.charAt(end++)]-- > 0) counter--; // in t
+            
+            while (counter == 0) { // valid
+                if (end - begin < d)  d = end - (head = begin);
+                if (map[s.charAt(begin++)]++ == 0) counter++; // make it invalid
+            }
+        }
+        
+        return d == Integer.MAX_VALUE ? "" : s.substring(head, head + d);
+    }
+}
