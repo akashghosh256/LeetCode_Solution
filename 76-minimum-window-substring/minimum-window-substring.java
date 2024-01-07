109c6f7258183302884fc4817759930395dffb38
Akash Ghosh <93257481+akashghosh256@users.noreply.github.com>
2024-01-07 17:09:26 +0530

    Time: 1328 ms (5.00%) | Memory: 45.9 MB (6.04%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -3,7 +3,7 @@
         
 //     }
 // }
-
+import java.util.*;
 
 public class Solution {
 
@@ -14,11 +14,20 @@ public class Solution {
         int counter = t.length(), begin = 0, end = 0, d = Integer.MAX_VALUE, head = 0;
         
         while (end < s.length()) {
+            System.out.println(s.charAt(end));
             if (map[s.charAt(end++)]-- > 0) counter--; // in t
+            System.out.println(" ---counter-> "+counter);
             
             while (counter == 0) { // valid
+             System.out.println("");
                 if (end - begin < d)  d = end - (head = begin);
-                if (map[s.charAt(begin++)]++ == 0) counter++; // make it invalid
+
+               System.out.println("--end->"+end+"--head->"+head+"--d-->"+d);
+
+                if (map[s.charAt(begin)]++ == 0) counter++; // make it invalid
+                begin++;
+                
+                 System.out.println("while 2 counter-->"+ counter+"--begin->"+begin);
             }
         }
         
