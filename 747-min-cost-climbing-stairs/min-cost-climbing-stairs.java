ab52bb079af4e8471e1093de4722e4936da405ba
Akash Ghosh <93257481+akashghosh256@users.noreply.github.com>
2024-06-10 11:19:10 +0530

    Time: 37 ms (8.21%) | Memory: 44.1 MB (5.89%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
@@ -1,8 +1,12 @@
 class Solution {
     public int minCostClimbingStairs(int[] cost) {
+     //   https://www.youtube.com/watch?v=ktmzAZWkEZ0&list=PLot-Xpze53lcvx_tjrr_m2lgD2NsRHlNO&index=38
         int n = cost.length;
-        int ar[] = new int[n+2];
-        ar[n] =0;
+        // creating new array to add goal index with value 0
+        int ar[] = new int[n+1];
+        // adding zero as goal value
+        ar[n] = 0;
+        // copying cost
         for( int i=0; i<n;i++){
             ar[i] = cost[i];
         }
@@ -12,6 +16,8 @@ class Solution {
         for(int i=m-3; i>=0;i--){
             ar[i] += Math.min(ar[i+1], ar[i+2]);
         }
+        for(int i : ar)
+        System.out.println(i);
 
         return Math.min(ar[0],ar[1]);
 
