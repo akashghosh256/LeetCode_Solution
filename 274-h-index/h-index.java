155fd0c70196fb606414c16119bb1a953eefd590
Akash Ghosh <93257481+akashghosh256@users.noreply.github.com>
2024-06-17 09:21:05 +0530

    Time: 2 ms (69.22%) | Memory: 41.2 MB (70.24%) - LeetSync

---
+++ 274-h-index/h-index.java
@@ -0,0 +1,14 @@
+class Solution {
+    // brute force
+    public int hIndex(int[] citations) {
+        int n = citations.length;
+        Arrays.sort(citations);
+        for (int i = 0; i < n; i++) {
+            int h = n - i;
+            if (citations[i] >= h) {
+                return h;
+            }
+        }
+        return 0;
+    }
+}
