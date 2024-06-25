208884bf4937bd0745975c558ce01c56cf94ddc9
Akash Ghosh <93257481+akashghosh256@users.noreply.github.com>
2024-06-25 11:03:44 +0530

    Time: 1 ms (99.61%) | Memory: 46.6 MB (25.23%) - LeetSync

--- 338-counting-bits/counting-bits.java
+++ 338-counting-bits/counting-bits.java
@@ -1,7 +1,7 @@
 class Solution {
 
     private int count(int n){
-        // Kernighan’s Algorithm
+        // Kernighan’s Algorithm =  log n
         int count =0;
         while(n>0){
              n = n & n-1;
@@ -13,14 +13,32 @@ class Solution {
 
     public int[] countBits(int n) {
 
-        // Kernighan’s Algorithm
+        // better Kernighan’s Algorithm o(n log n)
+
+        // int ans[] = new int[n+1];
+        // for(int i=0; i<= n; i++){
+        //     ans[i] = count(i);
+        // }
+
+        // return ans;
+
+
+
+        // using dp ayushi 
 
         int ans[] = new int[n+1];
-        for(int i=0; i<= n; i++){
-            ans[i] = count(i);
-        }
 
+        ans[0] = 0;
+
+        for(int i=1; i<=n ; i++){
+            if( i % 2 == 0)
+            ans[i] = ans[i/2];
+            else ans[i] = ans[i/2]+1;
+        }
+        
         return ans;
+
+    
 
         
     }
