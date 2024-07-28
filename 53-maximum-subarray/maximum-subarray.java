c3687f747fcabda8c8120b226c3396c2f632c3e8
Akash Ghosh <93257481+akashghosh256@users.noreply.github.com>
2024-07-28 19:27:59 +0530

    Time: 428 ms (6.48%) | Memory: 55.7 MB (99.68%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -1,21 +1,33 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        
-        int maxS = nums[0];
-        int sum =0;
-        for(int n : nums){
-            if( sum < 0){
-                sum =0;
-            }
+        // kadanes algo 
+        int ans = nums[0];
+        int sum=0;
+        int start = 0;
+        int end = 0;
 
-            sum += n;
-            maxS = Math.max(maxS, sum);
+        for(int i=0 ; i<nums.length; i++){
+            if( sum <0){
+                sum = 0;
+                start = i;
+            }
+            sum += nums[i];
+            if(sum > ans){
+                ans = sum;
+                end = i;
+            }
+            
+
         }
 
-        return maxS;
+     System.out.println("Longest subarray is ");
+        for(int i=start; i<=end; i++){
+            System.out.print(nums[i]+",");
+        }
 
 
 
+        return ans;
 
 
     }
