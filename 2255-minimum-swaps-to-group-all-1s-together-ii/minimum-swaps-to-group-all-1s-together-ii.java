d1684a9b1604b808dccb81b0c86873ccfd7376a9
Akash Ghosh <93257481+akashghosh256@users.noreply.github.com>
2024-08-03 12:31:46 +0530

    Time: 10 ms (47.21%) | Memory: 58.8 MB (82.74%) - LeetSync

--- 2255-minimum-swaps-to-group-all-1s-together-ii/minimum-swaps-to-group-all-1s-together-ii.java
+++ 2255-minimum-swaps-to-group-all-1s-together-ii/minimum-swaps-to-group-all-1s-together-ii.java
@@ -1,14 +1,57 @@
+// class Solution {
+//     public int minSwaps(int[] nums) {
+    
+
+//============================= sliding window =================================
+// https://www.youtube.com/watch?v=t82KJaI1kNQ&t=60s&ab_channel=codestorywithMIK
+
+
+//     int n = nums.length;
+//     int  totalOne = 0;
+//     int temp[] = new int[2*n];
+
+//     for(int i=0; i<(2*n); i++){
+//         temp[i] = nums[i%n];
+//         if(i<n && nums[i] == 1) totalOne++;
+//     }
+
+//     int i=0;
+//     int j=0;
+//     int curOne = 0;
+//     int maxOne = 0;
+
+//     while(j< 2*n){
+//         if(temp[j] == 1) curOne++;
+
+//         // maintain window
+//         if(j-i+1 > totalOne){
+//             curOne -= temp[i];
+//             i++;
+//         }
+
+//         maxOne = Math.max(maxOne, curOne);
+//         j++;
+//     }
+
+//         return  totalOne - maxOne;
+
+
+
+
+//     }
+// }
+
+// ========================= do with out temp array ==================
+
 class Solution {
     public int minSwaps(int[] nums) {
     
     int n = nums.length;
     int  totalOne = 0;
-    int temp[] = new int[2*n];
 
-    for(int i=0; i<(2*n); i++){
-        temp[i] = nums[i%n];
-        if(i<n && nums[i] == 1) totalOne++;
-    }
+    for(int i : nums)
+    if( i == 1) totalOne++;
+    
 
     int i=0;
     int j=0;
@@ -16,11 +59,13 @@ class Solution {
     int maxOne = 0;
 
     while(j< 2*n){
-        if(temp[j] == 1) curOne++;
+
+
+        if( nums[j%n] == 1) curOne++;
 
         // maintain window
         if(j-i+1 > totalOne){
-            curOne -= temp[i];
+            curOne -= nums[i%n];
             i++;
         }
 
