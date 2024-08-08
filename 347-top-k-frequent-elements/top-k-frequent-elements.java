9f3ef2a5065b0c1288c0ab3ea164d2cb1c97658a
Akash Ghosh <93257481+akashghosh256@users.noreply.github.com>
2024-08-08 22:21:34 +0530

    Time: 14 ms (55.16%) | Memory: 48.7 MB (39.79%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,27 +1,27 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        HashMap <Integer, Integer> map = new HashMap<>();
-        for(int i : nums){
-           map.put(i, map.getOrDefault(i,0)+1); 
+        Map<Integer, Integer> map = new HashMap<>();
 
+        for(int num : nums)
+        map.put(num,map.getOrDefault(num,0)+1);
+
+        PriorityQueue<Map.Entry<Integer,Integer>> minHeap = new PriorityQueue<>(
+            (a,b) -> a.getValue() - b.getValue()
+        );
+
+        for(Map.Entry<Integer,Integer> entry : map.entrySet()){
+            minHeap.add(entry);
+
+            if(minHeap.size() > k) minHeap.poll();
         }
+        int ans [] = new int [k];
+        int i=0;
+         while(!minHeap.isEmpty()){
+            ans[i++] = minHeap.poll().getKey();
+         }
 
-        PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<>(){
-            public int compare(int[]a , int[]b ){
-                return b[1] - a[1];
-            }
-        });
-
-        for(Map.Entry<Integer,Integer> entry : map.entrySet()){ 
-            int arr[] = {entry.getKey(), entry.getValue()};
-            pq.add(arr);
-        }
-        int ans[] = new int[k];
-        for(int i=0; i<k; i++){
-            ans[i] = pq.poll()[0];
-        }
-        return ans;
-
+         return ans;
+ 
         
     }
 }
