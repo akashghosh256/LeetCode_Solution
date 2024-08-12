38182df71760b887765767f09b025d879b6a1bcc
Akash Ghosh <93257481+akashghosh256@users.noreply.github.com>
2024-08-12 18:21:30 +0530

    Time: 2 ms (97.25%) | Memory: 43.3 MB (67.23%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,24 +1,36 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
 
-        if(s.length() != t.length()) return false;
+//     if( s.length() != t.length()) return false;
+//     HashMap <Character, Integer> map = new HashMap<>();
+//     HashMap <Character, Integer> map2 = new HashMap<>();
 
-        int alpha[] = new int [27];
+//  for (char c : s.toCharArray()) {
+//             map.put(c, map.getOrDefault(c, 0) + 1);
+//         }
+//      for (char c : t.toCharArray()) {
+//             map2.put(c, map2.getOrDefault(c, 0) + 1);
+//         }
 
-        for(char c : s.toCharArray())
-         alpha[c-'a']++;
+//     for( char c : s.toCharArray()){
+//         if(map.get(c) != map2.get(c)) return false;
+//     }
+//     return true;
+
+if( s.length() != t.length()) return false;
+int [] alphabet = new int[26];
+
+for(char c : s.toCharArray()){
+alphabet[c-'a']++;
+}
+
+for(char c : t.toCharArray()){
+   if(alphabet[c -'a'] > 0) alphabet[c -'a']--;
+   else return false;
+}
+
+return true;
 
 
-     for(char c : t.toCharArray()){
-         if( alpha[c-'a'] > 0) alpha[c-'a']--;
-         else return false;
-     }
-
-     return true;
-
-
-
-
-        
     }
 }
