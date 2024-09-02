26188352bf2175b43d9ba1928e57e8b3c42d3d26
Akash Ghosh <93257481+akashghosh256@users.noreply.github.com>
2024-09-02 18:45:15 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (6.29%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,25 +1,25 @@
 class Solution {
 
     // same as fibonacci series
-        static int f(int n , int dp[]){
-            if(n<=1) return 1;
+        // static int f(int n , int dp[]){
+        //     if(n<=1) return 1;
 
-            if( dp[n] != -1) return dp[n];
+        //     if( dp[n] != -1) return dp[n];
 
-            return dp[n]= f(n-1,dp)+f(n-2,dp);
+        //     return dp[n]= f(n-1,dp)+f(n-2,dp);
         
-        }
+        // }
 
     public int climbStairs(int n) {
         
         int dp[] = new int[n+1];
         Arrays.fill(dp,-1);
-        // dp[0] =1;
-        // dp[1]=1;
-        // for(int i=2;i<=n;i++){
-        //     dp[i] = dp[i-1]+dp[i-2];
-        // }
-     return (f(n,dp));
-        //return dp[n];
+        dp[0] =1;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i] = dp[i-1]+dp[i-2];
+        }
+        // return (f(n,dp));
+        return dp[n];
     }
 }
