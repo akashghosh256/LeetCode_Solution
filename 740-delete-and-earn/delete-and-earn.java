b467163f73cb363d749099449e9d5f6d56f838ad
Akash Ghosh <93257481+akashghosh256@users.noreply.github.com>
2024-09-15 12:55:42 +0530

    Time: 3 ms (85.61%) | Memory: 44.2 MB (84.08%) - LeetSync

--- 740-delete-and-earn/delete-and-earn.java
+++ 740-delete-and-earn/delete-and-earn.java
@@ -1,45 +1,14 @@
-import java.util.*;
-
 class Solution {
-
-    // Helper function to calculate maximum points
-    int helper(int points[], int i, int n, int dp[]) {
-        if (i >= n) return 0; // Corrected the condition (i >= n)
- 
-        if(dp[i] != -1) return dp[i];
-
-        // Pick the current element and move to i+2
-        int pick = points[i] + helper(points, i + 2, n, dp);
-
-        // Do not pick the current element and move to i+1
-        int notPick = helper(points, i + 1, n, dp);
-
-        // Return the maximum of the two choices
-        return dp[i] =  Math.max(pick, notPick);
-    }
-
     public int deleteAndEarn(int[] nums) {
-        int n = nums.length;
-        int maxE = 0;
-
-        // Find the maximum element in nums
-        for (int i : nums) {
-            maxE = Math.max(maxE, i);
+        int[] sum = new int[10001]; 
+        int max = 0;
+        for(int i = 0; i < nums.length; i++){
+            sum[nums[i]] += nums[i];
+            max = Math.max(max,nums[i]);
         }
-
-        // Create points array to store the sum of values
-        int points[] = new int[maxE + 1];
-        Arrays.fill(points, 0);
-
-        // Populate the points array
-        for (int i : nums) {
-            points[i] += i;
+        for(int i = 2; i <=max; i++){
+            sum[i] = Math.max(sum[i-1], sum[i-2] + sum[i]);
         }
-
-        int dp[] = new int[maxE + 1];
-        Arrays.fill(dp,-1);
-
-        // Call the helper function starting from index 1
-        return helper(points, 1, maxE + 1, dp);
+        return sum[max];
     }
 }
