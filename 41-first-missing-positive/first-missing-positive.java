358b6eed5be598de2db82c39454663021bb43bc0
Akash Ghosh <93257481+akashghosh256@users.noreply.github.com>
2024-09-21 20:34:32 +0530

    Time: 1 ms (100.00%) | Memory: 57.1 MB (59.01%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.java
+++ 41-first-missing-positive/first-missing-positive.java
@@ -1,24 +1,61 @@
+// class Solution {
+//     public int firstMissingPositive(int[] nums) {
+
+//         // Approach 1 
+//         // sc O(n) TC=O(n)
+
+//         int n = nums.length;
+//         boolean check[] = new boolean[n + 1];
+        
+//         for (int i = 0; i < n; i++) {
+//             if (nums[i] > 0 && nums[i] <= n) {
+//                 check[nums[i] - 1] = true;
+//             }
+//         }
+
+//         for (int i = 0; i < n; i++) {
+//             if (!check[i]) {
+//                 return i + 1;
+//             }
+//         }
+
+//         return n + 1;
+//     }
+// }
+
 class Solution {
     public int firstMissingPositive(int[] nums) {
 
-        // Approach 1 
-        // sc O(n) TC=O(n)
-
         int n = nums.length;
-        boolean check[] = new boolean[n + 1];
-        
+        boolean onePresent = false;
+
+        // Check if '1' is present and convert invalid numbers to 1
         for (int i = 0; i < n; i++) {
-            if (nums[i] > 0 && nums[i] <= n) {
-                check[nums[i] - 1] = true;
+            if (nums[i] == 1) onePresent = true;
+            // Replace numbers that are out of range or non-positive
+            if (nums[i] <= 0 || nums[i] > n) {
+                nums[i] = 1;
             }
         }
 
+        // If '1' is not present, return 1
+        if (!onePresent) return 1;
+
+        // Mark elements by setting the corresponding index to negative
         for (int i = 0; i < n; i++) {
-            if (!check[i]) {
+            int num = Math.abs(nums[i]);
+            if (num <= n) {
+                nums[num - 1] = -Math.abs(nums[num - 1]);
+            }
+        }
+
+        // Find the first index which is not negative
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > 0) {
                 return i + 1;
             }
         }
 
-        return n + 1;
+        return n + 1; // If all numbers from 1 to n are present
     }
 }
