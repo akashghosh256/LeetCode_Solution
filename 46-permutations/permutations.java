e5d76986d3f9c91df7fb6f662e89b93d802c5440
Akash Ghosh <93257481+akashghosh256@users.noreply.github.com>
2024-09-22 01:10:59 +0530

    Time: 3 ms (8.29%) | Memory: 44.5 MB (50.74%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,39 +1,34 @@
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
 class Solution {
-    List<List<Integer>> result = new ArrayList<>();
-    Set<Integer> st = new HashSet<>();
+    List<List<Integer>> ans = new ArrayList<>();
+   HashSet<Integer> st = new HashSet<>();
     int n;
+
+    void solve(List<Integer> temp, int[] nums){
+        if(temp.size() == n){
+            ans.add(new ArrayList<>(temp));
+            return;       
+        }
     
-    public void solve(List<Integer> temp, int[] nums) {
-        if (temp.size() == n) {
-            result.add(new ArrayList<>(temp));
-            return;
-        }
-        
-        for (int i = 0; i < n; i++) {
-            if (!st.contains(nums[i])) {
-                temp.add(nums[i]);
-                st.add(nums[i]);
 
-                solve(temp, nums);
+    for(int i=0; i<n; i++){
+        if(!st.contains(nums[i])){
+            st.add(nums[i]);
+            temp.add(nums[i]);
 
-                st.remove(nums[i]);
-                temp.remove(temp.size() - 1);
-            }
+            solve(temp, nums);
+
+            st.remove(nums[i]);
+            temp.remove(temp.size() -1);
         }
+
     }
-    
+    }
+
+
     public List<List<Integer>> permute(int[] nums) {
-        n = nums.length;
-        
-        List<Integer> temp = new ArrayList<>();
-        
-        solve(temp, nums);
-        
-        return result;
+    n = nums.length;
+    List<Integer> temp = new ArrayList<>();
+     solve(temp,nums);
+    return ans;
     }
 }
